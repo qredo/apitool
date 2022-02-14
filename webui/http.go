2cacfa4e90fe99c4eaa1f7af8d42321966c0f714
Ognyan Nikolov <ognyan@qredo.com>
2022-02-14 16:28:09 +0200

    clean up http router, disable debug prints

--- webui/http.go
+++ webui/http.go
@@ -9,18 +9,13 @@ import (
 )
 
 func Serve(port string) {
-	//gin.SetMode(gin.ReleaseMode)
+	gin.SetMode(gin.ReleaseMode)
 	router := gin.Default()
 	router.Use(cors.Default())
 
 	router.GET("/", GetIndex)
 	router.POST("/sign", Sign)
 	router.POST("/send", Send)
-	//router.PUT("/somePut", putting)
-	//router.DELETE("/someDelete", deleting)
-	//router.PATCH("/somePatch", patching)
-	//router.HEAD("/someHead", head)
-	//router.OPTIONS("/someOptions", options)
 
 	fmt.Printf("WebUI listening on http://127.0.0.1:%s\n", port)
 
